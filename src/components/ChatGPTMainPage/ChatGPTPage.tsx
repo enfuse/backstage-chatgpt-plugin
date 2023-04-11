888f0d8c8278839deff8a1415cee1ae0016f6501
ch-enfuse <chr@enfuse.io>
2023-04-11 13:57:54 -0700

    Changed page title

--- src/components/ChatGPTMainPage/ChatGPTPage.tsx
+++ src/components/ChatGPTMainPage/ChatGPTPage.tsx
@@ -4,20 +4,17 @@ import {
   Header,
   Page,
   Content,
-  ContentHeader,
   HeaderLabel,
 } from '@backstage/core-components';
 import { ChatGptPlayground } from '../ChatGptPlayground';
 
 export const ChatGPTPage = () => (
   <Page themeId='tool'>
-    <Header title="Welcome to chatgpt-frontend!" subtitle="Optional subtitle">
+    <Header title="ChatGPT Playground" subtitle="Select a frontend framework and describe the component you'd like to build ">
       <HeaderLabel label="Owner" value="Enfuse" />
       <HeaderLabel label="Lifecycle" value="Alpha" />
     </Header>
     <Content>
-      <ContentHeader title="ChatGPT Playground">
-      </ContentHeader>
       <Grid container spacing={3} direction="column">
         <Grid item>
           <ChatGptPlayground />
